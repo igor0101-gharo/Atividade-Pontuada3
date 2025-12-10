2df94fdc1dc76aa3e9787bead7aa535ca6c7b213
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-12-10 15:01:10 -0300

    Aulas

--- Questão1.py
+++ Questão1.py
@@ -23,12 +23,13 @@ def verificar_lista(lista):
     return False
 
 #Função para adicionar 4 aviões, feita de modo que ela só aceite números e peça novamente caso a pessoa digite um valor inválido
+#Caso já haja aviões cadastrados, a função mostra os aviões.
 def add_avioes(lista_aviao):
     if verificar_lista(lista_aviao):
         for i in range(4):
             while True:
                 try:
-                    aviao = int(input(f"Digite o número do {i+1}º Avião."))
+                    aviao = int(input(f"Digite o número do {i+1}º Avião.\n"))
                     lista_aviao.append(aviao)
                     os.system("cls")
                     break
@@ -40,17 +41,41 @@ def add_avioes(lista_aviao):
                     continue
     else:
         print("4 aviões já estão cadastrados. Reinicie o programa caso deseje recadastrar.")
+        print("---lista de aviões disponíveis---")
+        for i in lista_aviao():
+            print(f"-Avião {lista_aviao[i]}")
 
 def add_assentos(lista_assento, lista_aviao):
     if verificar_lista(lista_aviao):
         print("Ainda não há aviões cadastrados")
     
-    if verificar_lista(lista_assento):
+    elif verificar_lista(lista_assento):
         for i in range(4):
-            quantidade_assentos = int(input("Digite o número de assentos disponiveis"))
+            while True:
+                try:
+                    quantidade_assentos = int(input(f"Digite o número de assentos disponiveis no avião {lista_aviao[i]}\n"))
+                    lista_assento.append(quantidade_assentos)
+                    os.system("cls")
+                    break
+                except ValueError:
+                    print("Entrada inválida.")
+                    time.sleep(1)
+                    os.system("cls")
+                    continue
+    
+    else:
+        print("Quantidade de assentos em cada avião já cadastrada. Reinicie o programa caso queira recadastrar.")
+        print("---Lista de assentos disponíves")
+        for i in range(4):
+            print(f"-Avião: {lista_aviao[i]}")
+            print(f"Número de assentos disponíveis: {lista_assento[i]}\n\n")
 
 
-add_avioes(lista_aviao)
+
+            
+
+
+
 
             
 
