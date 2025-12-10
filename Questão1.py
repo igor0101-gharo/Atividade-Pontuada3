5620c3bcc85d6ffdbc5c74e713f600f203e3103e
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-12-10 14:33:38 -0300

    prova

--- Questão1.py
+++ Questão1.py
@@ -22,13 +22,44 @@ def verificar_lista(lista):
         return True
     return False
 
-def add_or_att_aviao(lista_aviao):
+#Função para adicionar 4 aviões, feita de modo que ela só aceite números e peça novamente caso a pessoa digite um valor inválido
+def add_avioes(lista_aviao):
     if verificar_lista(lista_aviao):
+        contador = 0
         for i in range(4):
             try:
                 aviao = int(input(f"Digite o número do {i+1}º Avião."))
                 lista_aviao.append(aviao)
-                os.ssytem("cls")
+                os.system("cls")
             except ValueError:
-                print("Entradad inválida. Digite um número inteiro.")
+                print("Entrada inválida.")
+                contador += 1 
+                time.sleep(1)
+                os.system("cls")
+                continue
+    if contador > 0:
+        print("houveram 1 ou mais aviões inválidos.")
+        while True:
+            try:
+                for i in range(contador):
+                    aviao = int(input("Digite o número do avião inválido novamente."))
+                    lista_aviao.append(aviao)
+                    contador = contador - 1
+                    os.system("cls")
+            except ValueError:
+                print("Entrada inválida.")
+                time.sleep(1)
+                os.system("cls")
+                continue
+            if contador == 0:
+                break
+
+
+
+add_avioes(lista_aviao)
+
+print(f"{lista_aviao}")
+
+
+            
 
